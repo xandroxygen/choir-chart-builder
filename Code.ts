9f5814c146c77d2dabbec14eabd4b6a46ae9ae2a
Xander Moffatt <xander.moffatt@gmail.com>
2019-08-05 08:28:50 -0700

    make buildRows and getSeatsPerRow work properly

--- Code.ts
+++ Code.ts
@@ -1,7 +1,7 @@
-import {SectionTitle, Row, SectionNumbers} from "./definitions"
+import { SectionTitle, Row, SectionNumbers } from "./definitions";
 
 function buildChart() {
-  const sheet = SpreadsheetApp.getActive().getSheetByName('Input');
+  const sheet = SpreadsheetApp.getActive().getSheetByName("Input");
   const inputData = sheet.getDataRange().getValues();
 }
 
@@ -11,58 +11,69 @@ function buildChart() {
  * @returns {number} The height in inches
  * @customfunction
  */
-function convertHeight(height: string) : number {
+function convertHeight(height: string): number {
   const [feet, inches] = height.split("-").map(s => parseFloat(s));
-  return (feet * 12) + inches;
+  return feet * 12 + inches;
 }
 
-export const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+export function buildRows(
+  total: number,
+  availableRows: number,
+  startingRow: string
+): Row[] {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const startingRowIndex = alphabet.indexOf(startingRow.toUpperCase());
 
-function buildRows(total: number, availableRows: number, startingRow: string): Row[] {
-  if (!alphabet.includes(startingRow)) {
-    throw Error("startingRow must be one upper-case letter");
+  if (startingRowIndex === -1) {
+    throw Error("startingRow must be one letter");
   }
 
-  const seatsPerRow = Math.floor(total / availableRows);  
-  const startingRowIndex = alphabet.indexOf(startingRow);
-
-  const rowLetters = alphabet.slice(startingRowIndex, startingRowIndex + availableRows);
-  return rowLetters.map(letter => ({ letter, seats: seatsPerRow} as Row));
+  const rowLetters = alphabet.slice(
+    startingRowIndex,
+    startingRowIndex + availableRows
+  );
+  return rowLetters.map(letter => ({ letter, seats: 0 } as Row));
 }
 
-function getSeatsPerRow(total: number, availableRows: number, rows: Row[]): Row[]
-{
-  const remainder = total % availableRows;
+export function getSeatsPerRow(total: number, rows: Row[]): Row[] {
+  const cRows = rows.length;
+  const seatsPerRow = Math.floor(total / cRows);
+  const remainder = total % cRows;
+
+  for (let i = 0; i < cRows; i++) {
+    rows[i].seats = seatsPerRow;
+  }
 
   // add 1 to the top {remainder} rows
-  for (let i = 0; i < remainder; i++)
-  {
-    rows[availableRows - i].seats += 1;
+  for (let i = 1; i < remainder + 1; i++) {
+    rows[cRows - i].seats += 1;
   }
 
   // create gaps of 2 between rows
-  for (let i = 0; i < availableRows; i++) {
+  for (let i = 0; i < cRows; i++) {
     // offset = (i - middle index) * 2
     // ^ this works for arrays with odd length
     // offset = ((i - index of middle floored) * 2) - 1
-    const middleIndex = Math.floor(availableRows / 2);
-    
+    const middleIndex = Math.floor(cRows / 2);
+
     // for odd-length arrays, the middle index should be offset at 0
     // for even-length arrays, the "middle" index should be floored,
     // and the offset should start at -1
-    const offset = ((i - middleIndex) * 2) - 1;
-    
+    const offset = (i - middleIndex) * 2 - 1;
+
     rows[i].seats += offset;
   }
 
   // check for overflow beyond max row size
   // start with the top row and bump down overflow
   const maxRowSize = 36;
-  for (let i = availableRows; i >= 0; i--) {
+  for (let i = cRows - 1; i >= 0; i--) {
     if (rows[i].seats > maxRowSize) {
       if (i == 0) {
         // if the bottom row is overflowing, we have a problem
-        throw new Error("Number of rows and number of available seats isn't enough to contain everyone.");
+        throw new Error(
+          "Number of rows and number of available seats isn't enough to contain everyone."
+        );
       }
 
       // bump overflow down to the previous row
@@ -72,39 +83,25 @@ function getSeatsPerRow(total: number, availableRows: number, rows: Row[]): Row[
     }
   }
 
-  // move the 3-gap to between second and third
-  // row, if there is one
-  // start at the top and don't check the bottom row
-  const gapToSwitch = rows[2].seats - rows[1].seats;
-  for (let i = availableRows; i > 0; i--) {
-    const gap = rows[i].seats - rows[i-1].seats;
-    if (gap > 2 && i != 2) {
-      const difference = gap - gapToSwitch;
-
-    }
-  }
-  
-  
-
-
-  return [];
+  return rows;
 }
 
 function getSeatsFromSection(sectionCounts: SectionNumbers[], rows: Row[]) {
-  const total: number = sectionCounts.reduce((total, section) => total + section.count, 0);
-  const proportions: number[] = sectionCounts.map(section => section.count / total);
-  
-  const sectionStacks: number[][] = proportions.map(proportion => 
+  const total: number = sectionCounts.reduce(
+    (total, section) => total + section.count,
+    0
+  );
+  const proportions: number[] = sectionCounts.map(
+    section => section.count / total
+  );
+
+  const sectionStacks: number[][] = proportions.map(proportion =>
     rows.map(row => proportion * row.seats)
   );
 
   for (let i = 0; i < rows.length; i++) {
-    const isRowFull: boolean = sectionStacks.reduce((total, stack) => total + stack[i], 0) === rows[i].seats;
+    const isRowFull: boolean =
+      sectionStacks.reduce((total, stack) => total + stack[i], 0) ===
+      rows[i].seats;
   }
-}
-
-function confirmRowCounts() {
-  const sheet = SpreadsheetApp.getActive().getSheetByName('Confirmation');
-  const rowCounts = sheet.getRange('A2:A7').getValues();
-  sheet.getRange('C2:C7').setValues(rowCounts);
 }
